3b41e8191db14134a7bcd5df4c0642d5d6bfd808
Gabriel Sol <gabsolsilva@gmail.com>
2021-06-02 22:35:24 -0300

    ex 4

--- Assignment-2/Test_binary_tree.py
+++ Assignment-2/Test_binary_tree.py
@@ -1,34 +1,20 @@
-import Test_binary_tree as t, unittest
+import Binary_tree as t, unittest
 
 class TestPart1(unittest.TestCase):
 
-    def test_binarytree_insert_ex1(self):
-        tree = t.Node(16)
-        t.insert(tree, 10)
-        t.insert(tree, 21)
-        t.insert(tree, 7)
-        t.insert(tree, 18)
-        t.insert(tree, 29)
-        t.insert(tree, 99)
-        t.insert(tree, 13)
-        print("\nTest 1")
-        tree.print()
-
     def test_binarytree_find_ex2(self):
-        print("\nTest 2")
-        tree = t.Node(16)
-        t.insert(tree, 10)
-        t.insert(tree, 21)
-        t.insert(tree, 7)
-        t.insert(tree, 18)
-        t.insert(tree, 29)
-        t.insert(tree, 99)
-        t.insert(tree, 13)
-        #tree.print()
+        tree = t.BinaryTree(16)
+        tree.insert(10)
+        tree.insert(21)
+        tree.insert(7)
+        tree.insert(18)
+        tree.insert(29)
+        tree.insert(99)
+        tree.insert(13)
         for i in range(9, 100):
-            result = t.find(tree, i)
+            result = tree.find(i)
             if result:
-                print(i, result)
+                self.assertEqual(True, result)
 
 if __name__ == '__main__':
     unittest.main()
