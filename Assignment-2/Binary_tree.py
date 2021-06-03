1cfe1b605bbf1beb8a7acbd41f81c98c81013ce7
Gabriel Sol <gabsolsilva@gmail.com>
2021-06-02 22:35:06 -0300

    ex 4

--- Assignment-2/Binary_tree.py
+++ Assignment-2/Binary_tree.py
@@ -1,56 +1,47 @@
-class Node:
+class BinaryTree:
     def __init__(self, key):
         self.key = key
         self.left = None
         self.right = None
 
-    def print(self):
-        print(self.key)
-        if self.right is not None:
-            self.right.print()
-        if self.left is not None:
-            self.left.print()
-            
+    def insert(self, key):
+        if self.key:
+            if key > self.key:
+                if self.right is None:
+                    node = BinaryTree(key)
+                    self.right = node
+                else:
+                    self.right.insert(key)
+            else:
+                if self.left is None:
+                    node = BinaryTree(key)
+                    self.left = node
+                else:
+                    self.left.insert(key)
 
-def insert(root, key):
-    if key > root.key:
-        if root.right is None:
-            node = Node(key)
-            root.right = node
-        else:
-            insert(root.right, key)
-    else:
-        if root.left is None:
-            node = Node(key)
-            root.left = node
-        else:
-            insert(root.left, key)
+    def find(self, key):
+        if key == self.key:
+            return True
 
-def find(root, key):
-    if key == root.key:
-        return True
-
-    elif key > root.key:
-        if root.right is None:
-            return False
+        elif key > self.key:
+            if self.right is None:
+                return False
+            else:
+                return self.right.find(key)     
         else:
-            return find(root.right, key)     
-    else:
-        if root.left is None:
-            return False
-        else:
-            return find(root.left, key)
-        
+            if self.left is None:
+                return False
+            else:
+                return self.left.find(key)
 
+                
 if __name__ == "__main__":
-    tree = Node(16)
-    insert(tree, 10)
-    insert(tree, 21)
-    insert(tree, 7)
-    insert(tree, 18)
-    insert(tree, 29)
-    insert(tree, 99)
-    insert(tree, 13)
-    for i in range(9, 100):
-        result = find(tree, i)
-        print(i, result)
+    tree = BinaryTree(16)
+    tree.insert(10)
+    tree.insert(21)
+    tree.insert(7)
+    tree.insert(18)
+    tree.insert(29)
+    tree.insert(99)
+    tree.insert(13)
+    print(tree.find())
